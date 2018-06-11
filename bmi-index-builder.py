db15661b21ab6b096cd6ad8d7c1d99417ebdab26
George Ponick <gponick@gmail.com>
2018-06-11 03:38:25 +0000

    Update modlist.json by bmi-index-builder. Add 3 starting mods

---
+++ bmi-index-builder.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import logging
+import requests
+import os
+import pickle
+import time
+import json
+
+allfiles = os.listdir('.')
+mods = []
+for fil in allfiles:
+    if fil == ".git":
+        continue
+    if os.path.isdir(os.path.join(os.path.abspath('.'), fil)):
+        with open(os.path.join(os.path.abspath('.'),fil,'mod.json'),'r') as modfile:
+            modjson = modfile.read()
+            mod = json.loads(modjson)
+            mod['Name'] = fil
+            mods.append(mod)
+
+modlistdict = {}
+
+for mod in mods:
+    modlistdict[mod['Name']] = mod
+
+os.remove(os.path.join(os.path.abspath('.'),'modlist.json'))
+with open(os.path.join(os.path.abspath('.'),'modlist.json'),'w') as modlistfile:
+    modlistfile.write(json.dumps(modlistdict, indent=4))
+        
+print(json.dumps(modlistdict, indent=4))
